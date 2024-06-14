7cf48c816a4a65ddfc589f3f1221afeb17ae0248
Huy Nguyễn Vũ <nguyenvuhuy2110@gmail.com>
2024-06-14 16:00:07 +0700

    code 1406

--- app/Http/Controllers/SanPhamDaiLyController.php
+++ app/Http/Controllers/SanPhamDaiLyController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\SanPham;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SanPhamDaiLyController extends Controller
 {
     public function getData()
     {
-        $data = SanPham::get();
+        $user_login = Auth::guard('sanctum')->user();
+        $data = SanPham::where('id_dai_ly', $user_login->id)->get();
 
         return response()->json([
             'data' => $data
@@ -18,16 +20,18 @@ class SanPhamDaiLyController extends Controller
 
     public function store(Request $request)
     {
+        $user_login = Auth::guard('sanctum')->user();
         SanPham::create([
-            'ten_san_pham'  =>$request->ten_san_pham,
-            'slug_san_pham'  =>$request->slug_san_pham,
-            'so_luong'   =>$request->so_luong,
-            'hinh_anh'   =>$request->hinh_anh,
-            'mo_ta_ngan'   =>$request->mo_ta_ngan,
-            'mo_ta_chi_tiet'   =>$request->mo_ta_chi_tiet,
-            'tinh_trang'  =>$request->tinh_trang,
-            'gia_ban'  =>$request->gia_ban,
-            'gia_khuyen_mai'  =>$request->gia_khuyen_mai,
+            'ten_san_pham'      =>  $request->ten_san_pham,
+            'slug_san_pham'     =>  $request->slug_san_pham,
+            'so_luong'          =>  $request->so_luong,
+            'hinh_anh'          =>  $request->hinh_anh,
+            'mo_ta_ngan'        =>  $request->mo_ta_ngan,
+            'mo_ta_chi_tiet'    =>  $request->mo_ta_chi_tiet,
+            'tinh_trang'        =>  $request->tinh_trang,
+            'gia_ban'           =>  $request->gia_ban,
+            'gia_khuyen_mai'    =>  $request->gia_khuyen_mai,
+            'id_dai_ly'         =>  $user_login->id,
         ]);
         return response()->json([
             'status' => true,
@@ -37,23 +41,35 @@ class SanPhamDaiLyController extends Controller
 
     public function xoaSP(Request $request)
     {
-        SanPham::find($request->id)->delete();
-        return response()->json([
-            'status' => true,
-            'message' => "Đã xóa sản phẩn". $request->ten_san_pham . " thành công.",
-        ]);
+        $user_login = Auth::guard('sanctum')->user();
+        $check = SanPham::where('id', $request->id)->where('id_dai_ly', $user_login->id)->delete();
+        if ($check) {
+            return response()->json([
+                'status' => true,
+                'message' => "Đã xóa sản phẩn". $request->ten_san_pham . " thành công.",
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => "Xóa sản phẩm thất bại",
+            ]);
+        }
+        
+        
     }
 
     public function chuyenTrangThaiBan(Request $request)
     {
+        $user_login = Auth::guard('sanctum')->user();
         $tinh_trang = $request->tinh_trang == 1 ? 0 : 1;
-        SanPham::find($request->id)->update([
+        $check = SanPham::where('id', $request->id)->where('id_dai_ly', $user_login->id)->update([
             'tinh_trang'    =>  $tinh_trang
         ]);
 
         return response()->json([
-            'status' => true,
-            'message' => "Đã đổi tình trạng sản phẩm". $request->ten_san_pham . " thành công.",
+            'status'    =>  $check,
+            'message'   => 
+                        $check == true ? "Đã đổi tình trạng sản phẩm". $request->ten_san_pham . " thành công." : "Thay đổi trạng thái thất bại",
         ]);
     }
 
@@ -73,19 +89,6 @@ class SanPhamDaiLyController extends Controller
         return response()->json([
             'status' => true,
             'message' => "Đã sửa đổi thông tin ". $request->ten_san_pham . " thành công.",
-        ]);
-    }
-
-    public function chuyenFlashSale(Request $request)
-    {
-        $is_flash_sale = $request->is_flash_sale == 1 ? 0 : 1;
-        SanPham::find($request->id)->update([
-            'is_flash_sale'    =>  $is_flash_sale
-        ]);
-
-        return response()->json([
-            'status' => true,
-            'message' => "Đã đổi tình trạng sản phẩm". $request->ten_san_pham . " thành công.",
         ]);
     }
 }
